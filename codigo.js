c64acb07919e463c29f338badcace4189e455970
Carlos Francisco Ruiz Diaz <carlosfranciscord@gmail.com>
2024-05-05 18:24:02 -0300

    cambios con rubrica 3

--- codigo.js
+++ codigo.js
@@ -1,65 +1,81 @@
-let funcionalidad = parseInt(prompt('Ingresa la opcion deseada\n1-Filtrar por prioridad de vencimiento\n2-Buscar vencimiento por nombre\n3-Mostrar cantidad de vencimientos pendientes\n0-PARA SALIR'));
+console.table(Productos);
+let carrito = []
 
+const articuloCartas = document.getElementById("carta");
+articuloCartas.className = "row container gap-3 mx-auto my-3"
+const tablaBody = document.getElementById('tablabody');
 
-while (funcionalidad != 0) {
-    switch (funcionalidad) {
-        case 1:
-            //filtrado
-            let opcion = parseInt(prompt('Visualizar vencimientos por prioridad\n1-Alta\n2-Media\n3-Baja'));
-            filtrarPorPrioridad(opcion);
-            break;
-        case 2:
-            //busqueda por nombre de vencimiento
-            let nombre = prompt('Ingresa el nombre de la vencimiento a buscar');
-            if (nombre != '') {
-                buscarTarea(nombre);
-            } else {
-                alert('No ingresaste ningun vencimiento')
-            }
-            break;
-        case 3:
-            //funcion que cuenta la cantidad de vencimientos pendientes
-            contarTareas();
-            break;
-        default:
-            alert('Opcion invalida!');
-            break;
+function renderizarProductos(listProds){
+    for (const prod of listProds){
+        articuloCartas.innerHTML+=`
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src=${prod.foto} alt=${prod.foto}>
+            <div class="card-body">
+                <h5 class="card-title">${prod.nombre}</h5>
+                <p class="card-text">${prod.precio}</p>
+                <button class="btn btn-primary compra" id=${prod.id}>Agregar</button>
+            </div>
+        </div>
+        `
     }
-
-    funcionalidad = parseInt(prompt('Ingresa la opcion deseada\n1-Filtrar por prioridad de vencimiento\n2-Buscar vencimiento por nombre\n3-Mostrar cantidad de vencimientos pendientes\n0-PARA SALIR'));
-}
-
-
-function filtrarPorPrioridad(prioridad) {
-    let filtrados = [];
-    if (prioridad == 1) {
-        filtrados = tareas.filter(tarea => tarea.prioridad.toLowerCase() == "alta");
-    } else if (prioridad == 2) {
-        filtrados = tareas.filter(tarea => tarea.prioridad.toLowerCase() == "media");
-    } else if (prioridad == 3) {
-        filtrados = tareas.filter(tarea => tarea.prioridad.toLowerCase() == "baja");
-    } else {
-        alert('prioridad invalida');
-    }
-    console.table(filtrados);
-}
-
-
-function buscarTarea(nomTarea) {
-    let tareaEncontrada = tareas.find(tarea => tarea.nombre.toLowerCase().includes(nomTarea.toLowerCase()));
-
-    if (tareaEncontrada != undefined) {
-        alert(`La tarea fue encontrada:\nID:${tareaEncontrada.id} \nNOMBRE:${tareaEncontrada.nombre} \nDURACION: ${tareaEncontrada.duracion} \nPRIORIDAD: ${tareaEncontrada.prioridad} \nFECHA: ${tareaEncontrada.fecha.toLocaleDateString()}`);
-    } else {
-        console.log('Tarea no encontrada');
+// eventos
+    const botonesCompra = document.getElementsByClassName('compra');
+    for(const boton of botonesCompra){
+        boton.addEventListener('click', ()=>{
+            console.log('Hiciste click en el boton cuyo id es: '+boton.id); 
+            const prodACarrito = listProds.find(prod => prod.id == boton.id); 
+            console.log(prodACarrito);
+            agregarACarrito(prodACarrito);
+        })
     }
 }
+renderizarProductos(Productos)
 
-
-function contarTareas() {
-    alert('Cantidad de tareas pendientes: ' + tareas.length);
+function agregarACarrito(producto){
+    carrito.push(producto);
+    console.table(carrito);
+    tablaBody.innerHTML+=`
+    <tr>
+        <td>${producto.id}</td>
+        <td>${producto.nombre}</td>
+        <td>${producto.precio}</td>
+    </tr>
+    `
+// agregar calculo total
 }
 
+// eventos teclado
 
+const campoNombre = document.getElementById('nombre');
+const campoEmail = document.getElementById('email');
 
+campoNombre.onkeyup =() =>{
+    if(campoNombre.value.length < 3){
+        console.log('nombre de menos de 3 letras'); 
+        campoNombre.style.color='red';
+    }else{
+        campoNombre.style.color='black';
+    }
+}
+campoEmail.addEventListener('input', ()=>{
+    if((!campoEmail.value.includes('@')) ||(!campoEmail.value.includes('.'))){
+        document.getElementById('mensaje').innerText='ingrese un email valido !'
+    }else{
+        document.getElementById('mensaje').innerText=''
+    }
+})
 
+function borrarC(){
+    campoNombre.value = '';
+    campoEmail.value = '';
+}
+
+const formulario = document.getElementById('formulario');
+formulario.addEventListener('submit', validar);
+
+function validar (ev){
+    if((campoNombre.value == '') || (campoEmail.value == '')){
+        ev.preventDefault();
+        alert('ingrese nombre o email valido');
+    }
+}
---
+++ productos.js
@@ -0,0 +1,39 @@
+const Productos = [
+    {
+        id: 1,
+        nombre: 'Picada',
+        foto: 'https://media.istockphoto.com/id/1422816311/es/foto/tabla-de-cortar-madera-con-quesos-embutidos-y-mermeladas.jpg?s=1024x1024&w=is&k=20&c=3WSxwOwkC4qZzk7zVfocpHlospBaXhPCWjE5IpdcbDI=',
+        precio: 2000
+    },
+    {
+        id: 2,
+        nombre: 'Picada',
+        foto: 'https://media.istockphoto.com/id/1422816311/es/foto/tabla-de-cortar-madera-con-quesos-embutidos-y-mermeladas.jpg?s=1024x1024&w=is&k=20&c=3WSxwOwkC4qZzk7zVfocpHlospBaXhPCWjE5IpdcbDI=',
+        precio: 3210
+    },
+    {
+        id: 3,
+        nombre: 'Picada',
+        foto: 'https://media.istockphoto.com/id/1422816311/es/foto/tabla-de-cortar-madera-con-quesos-embutidos-y-mermeladas.jpg?s=1024x1024&w=is&k=20&c=3WSxwOwkC4qZzk7zVfocpHlospBaXhPCWjE5IpdcbDI=',
+        precio: 1000
+    },
+    {
+        id: 4,
+        nombre: 'Picada',
+        foto: 'https://media.istockphoto.com/id/1422816311/es/foto/tabla-de-cortar-madera-con-quesos-embutidos-y-mermeladas.jpg?s=1024x1024&w=is&k=20&c=3WSxwOwkC4qZzk7zVfocpHlospBaXhPCWjE5IpdcbDI=',
+        precio: 5623
+    },
+    {
+        id: 5,
+        nombre: 'Picada',
+        foto: 'https://media.istockphoto.com/id/1422816311/es/foto/tabla-de-cortar-madera-con-quesos-embutidos-y-mermeladas.jpg?s=1024x1024&w=is&k=20&c=3WSxwOwkC4qZzk7zVfocpHlospBaXhPCWjE5IpdcbDI=',
+        precio: 9800
+    },
+    {
+        id: 6,
+        nombre: 'Picada',
+        foto: 'https://media.istockphoto.com/id/1422816311/es/foto/tabla-de-cortar-madera-con-quesos-embutidos-y-mermeladas.jpg?s=1024x1024&w=is&k=20&c=3WSxwOwkC4qZzk7zVfocpHlospBaXhPCWjE5IpdcbDI=',
+        precio: 4562
+        
+    },
+]
